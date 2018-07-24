554dcb963febffcc0ea1e5df4105a200bc59b6cd
zhaopengtao <zhaopengtao@51talk.com>
2018-07-24 11:44:06 +0800

    zif

--- zifuchuan.php
+++ zifuchuan.php
@@ -13,9 +13,15 @@ $str = "123abc"; //不是数字
 if(is_numeric($str)){
 	var_dump("是数字");
 }
-
 var_dump(is_numeric($str));
 var_dump(is_int($str));
-
 var_dump(preg_match("/^\d*$/","123")); // 1
 var_dump(preg_match("/^\d*$/",$str));  // 0
+数学运算 不一定能判断是 字符串
+var_dump("123abc"); //123
+var_dump("abc123"); // 0
+乘法运算
+var_dump("123abc"*1); //123
+var_dump("abc123" *1); //0
+
+3.字符串截取
